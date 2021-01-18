892ac3dcf5cc734722fcd0645b2b1c70e62ae9a5
Souharda Biswas <mail2souharda2015@gmail.com>
2021-01-18 10:09:22 +0530

    Password_JS-5

--- app.js
+++ app.js
@@ -26,6 +26,22 @@ generate.addEventListener("click", () => {
   );
 });
 
+clipboard.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard");
+});
+
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
